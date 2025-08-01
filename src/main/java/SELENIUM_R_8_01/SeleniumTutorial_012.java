199e4ca962c76c13c148d87573680a3be51a0d0e
SUJIT <sujitmanmode1997@gmail.com>
2025-08-01 14:00:29 +0530

    second commit

--- src/main/java/SELENIUM_R_8_01/SeleniumTutorial_012.java
+++ src/main/java/SELENIUM_R_8_01/SeleniumTutorial_012.java
@@ -1,7 +1,35 @@
 package SELENIUM_R_8_01;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class SeleniumTutorial_012 {
+    public static String browser = "chrome";
+    public static WebDriver driver;
     public static void main(String[] args) {
+    if(browser.equals("chrome")){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    } else if (browser.equals("Edge")) {
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
+    } else if (browser.equals("forefox")) {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+    }
+    driver.manage().window().maximize();
+    driver.get("https://www.krushistore.com/");
+
+    //LOCATORS
+    driver.findElement(By.linkText("Solar Fence")).click();
+    driver.findElement(By.xpath("//*[@id='app-header']/div[2]/div/div[2]/div[2]/div/div[1]")).click();
+    // driver.findElement(By.xpath("//*[contains(@class, 'w-[29px]')]")).click();
+
+
 
     }
 }
