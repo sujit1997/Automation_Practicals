b36630196d44e090a4e00c496919ab1b1d809476
sujit <sujitmanmode1997@gmail.com>
2025-10-13 10:34:19 +0530

    todays update 13/10/25

---
+++ src/main/java/SELENIUM_/SMPS_SEL_26_MouseHover.java
@@ -0,0 +1,30 @@
+package SELENIUM_;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SMPS_SEL_26_MouseHover {
+    public static String browser = "chrome";
+    public static WebDriver driver;
+
+    public static void main(String[] args) {
+        //  Setup browser driver
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }
+        driver.manage().window().maximize();
+        driver.get("https://www.amazon.in/");
+
+
+    }
+}
