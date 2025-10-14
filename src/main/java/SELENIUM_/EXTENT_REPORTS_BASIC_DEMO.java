9b1667de2dfd1a5b8abe168a35a23a4be269e018
sujit <sujitmanmode1997@gmail.com>
2025-10-14 14:00:36 +0530

    todays update 14/10/25

---
+++ src/main/java/SELENIUM_/EXTENT_REPORTS_BASIC_DEMO.java
@@ -0,0 +1,16 @@
+package SELENIUM_;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
+public class EXTENT_REPORTS_BASIC_DEMO {
+//    public static void main(String[] args) {
+//        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("extentReports.html");
+//
+//        ExtentReports extent = new ExtentReports();
+//        extent.attachReporter(htmlReporter);
+//
+//        ExtentTest test1 = extent.createTest("MyFirstTest","Sample description");
+
+    }
+
