79aba2bc0009ffd12922694316d4f6a4e9658e96
sujit <sujitmanmode1997@gmail.com>
2025-10-14 10:19:05 +0530

    todays update 14/10/25

---
+++ src/test/java/TESTNG/TESTNG_9_Annotation.java
@@ -0,0 +1,15 @@
+package TESTNG;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TESTNG_9_Annotation {
+    @BeforeSuite
+    public void datasetup(){
+        System.out.println("commonn data setup");
+    }
+    @AfterSuite
+    public void datateardown(){
+        System.out.println("tear down");
+    }
+}
